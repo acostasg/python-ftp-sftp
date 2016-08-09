28723d60a15a051fc1df73ae2c4674a1792f6dff
Albert Costas <albert.costas@atrapalo.com>
2016-08-09 23:24:37 +0200

    Modify travis.yml

--- setup.py
+++ setup.py
@@ -63,13 +63,13 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    py_modules=["upload"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['importlib', 'pyyaml', 'pysftp', 'mock', 'patch'],
+    install_requires=['importlib', 'pyyaml', 'pysftp'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
